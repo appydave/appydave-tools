48d5f9db724c0e3cc7dcd70c8a4e43758be96b4b
David Cruwys <david@ideasmen.com.au>
2024-05-07 19:56:51 +0700

    chore: opinionated gem files

--- lib/appydave/tools/version.rb
+++ lib/appydave/tools/version.rb
@@ -2,6 +2,6 @@
 
 module Appydave
   module Tools
-    VERSION = "0.1.0"
+    VERSION = '0.0.1'
   end
 end
--- spec/appydave/tools_spec.rb
+++ spec/appydave/tools_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe Appydave::Tools do
-  it "has a version number" do
-    expect(Appydave::Tools::VERSION).not_to be nil
+  it 'has a version number' do
+    expect(Appydave::Tools::VERSION).not_to be_nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'has a standard error' do
+    expect { raise Appydave::Tools::Error, 'some message' }
+      .to raise_error('some message')
   end
 end
