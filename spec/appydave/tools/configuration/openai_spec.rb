30a114c7cd0085ab92aeb906e1962ca3618631e8
David Cruwys <david@ideasmen.com.au>
2024-05-26 10:48:36 +0700

    fix: remove from ci for now

--- spec/appydave/tools/configuration/openai_spec.rb
+++ spec/appydave/tools/configuration/openai_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe 'OpenAI configuration setup' do
-  # NOT working in CI
-  # it 'has openai access token' do
-  #   expect(OpenAI.configuration.access_token).not_to be_nil
-  # end
+  xit 'has openai access token' do
+    expect(OpenAI.configuration.access_token).not_to be_nil
+  end
 
-  # it 'has openai organization id' do
-  #   expect(OpenAI.configuration.organization_id).not_to be_nil
-  # end
+  xit 'has openai organization id' do
+    expect(OpenAI.configuration.organization_id).not_to be_nil
+  end
 end
